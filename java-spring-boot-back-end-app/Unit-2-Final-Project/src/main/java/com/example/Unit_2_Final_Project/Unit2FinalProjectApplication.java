bb119cda7e603e3716752fc8376180c5538a493a
rionazv <70412011+rionazv@users.noreply.github.com>
2026-09-10 17:06:09 -0500

    Initial commit.

---
+++ java-spring-boot-back-end-app/Unit-2-Final-Project/src/main/java/com/example/Unit_2_Final_Project/Unit2FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unit_2_Final_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2FinalProjectApplication.class, args);
+	}
+
+}
---
+++ java-spring-boot-back-end-app/Unit-2-Final-Project/src/test/java/com/example/Unit_2_Final_Project/Unit2FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Unit_2_Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
