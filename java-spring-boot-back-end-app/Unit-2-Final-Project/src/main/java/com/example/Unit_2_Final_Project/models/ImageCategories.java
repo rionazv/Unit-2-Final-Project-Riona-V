d1e19c1a0dbbfd843551a72a127a3c24ae4d7b93
rionazv <70412011+rionazv@users.noreply.github.com>
2026-09-13 15:32:22 -0500

    Created models for images, image sets, and image categories.

---
+++ java-spring-boot-back-end-app/Unit-2-Final-Project/src/main/java/com/example/Unit_2_Final_Project/models/ImageCategories.java
@@ -0,0 +1,37 @@
+package com.example.Unit_2_Final_Project.models;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "image_categories")
+public class ImageCategories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    // Constructors
+    public ImageCategories() {}
+
+    public ImageCategories(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}
