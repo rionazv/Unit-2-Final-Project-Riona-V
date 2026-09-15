c468efc1d3ccb9ac7e1cecbd4c8f40cdea1ca7bd
rionazv <70412011+rionazv@users.noreply.github.com>
2026-09-15 17:31:07 -0500

    Added helper methods and more relational mapping

--- Unit-2-Final-Project/Unit-2-Final-Project/src/main/java/com/hyperion_hoard/Unit_2_Final_Project/models/ImageCategories.java
+++ Unit-2-Final-Project/Unit-2-Final-Project/src/main/java/com/hyperion_hoard/Unit_2_Final_Project/models/ImageCategories.java
@@ -1,6 +1,7 @@
 package com.hyperion_hoard.Unit_2_Final_Project.models;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -15,13 +16,18 @@ public class ImageCategories {
     private String categoryName;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "imageCategory", orphanRemoval = true)
-    private List<Image> images;
+    private List<ImageSets> imageSets = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "imageCategory", orphanRemoval = true)
+    private List<Image> images = new ArrayList<>();
 
     // Constructors
     public ImageCategories() {}
 
-    public ImageCategories(String categoryName) {
+    public ImageCategories(String categoryName, List<ImageSets> imageSets, List<Image> images) {
         this.categoryName = categoryName;
+        this.imageSets = imageSets;
+        this.images = images;
     }
 
     // Getters and setters
@@ -41,5 +47,22 @@ public class ImageCategories {
 
     public List<Image> getImages() { return images; }
 
+    public List<ImageSets> getImageSets() { return imageSets; }
+
+    public void setImageSets(List<ImageSets> imageSets) { this.imageSets = imageSets; }
+
     public void setImages(List<Image> images) { this.images = images; }
+
+    // Helper method to add an image set to the category
+    public void addImageSet(ImageSets imageSet) {
+        imageSets.add(imageSet);
+        imageSet.setImageCategory(this);
+    }
+
+    // Helper method to add an image to the category
+    public void addImage(Image image) {
+        images.add(image);
+        image.setImageCategory(this);
+    }
+
 }
--- Unit-2-Final-Project/Unit-2-Final-Project/src/main/java/com/hyperion_hoard/Unit_2_Final_Project/models/ImageSets.java
+++ Unit-2-Final-Project/Unit-2-Final-Project/src/main/java/com/hyperion_hoard/Unit_2_Final_Project/models/ImageSets.java
@@ -1,6 +1,8 @@
 package com.hyperion_hoard.Unit_2_Final_Project.models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -14,14 +16,20 @@ public class ImageSets {
     @Column(name = "set_name")
     private String setName;
 
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_category_id")
+    private ImageCategories imageCategory;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "imageSet", orphanRemoval = true)
-    private List<Image> images;
+    private List<Image> images = new ArrayList<>();
 
     // Constructors
     public ImageSets() {}
 
-    public ImageSets(String setName) {
+    public ImageSets(String setName, ImageCategories imageCategory) {
         this.setName = setName;
+        this.imageCategory = imageCategory;
     }
 
     // Getters and setters
@@ -31,12 +39,22 @@ public class ImageSets {
 
     public void setId(int id) { this.id = id; }
 
-    public String getSetName() {
-        return setName;
-    }
+    public String getSetName() { return setName; }
 
     public void setSetName(String setName) {
         this.setName = setName;
+    }
+
+    public List<Image> getImages() { return images; }
+
+    public ImageCategories getImageCategory() { return imageCategory; }
+
+    public void setImageCategory(ImageCategories imageCategory) { this.imageCategory = imageCategory; }
+
+    // Helper method to add an image to the set
+    public void addImage(Image image) {
+        images.add(image);
+        image.setImageSet(this);
     }
 
 }
