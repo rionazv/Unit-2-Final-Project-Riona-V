f064149d9a16eccd048410a9f6d615b076b41854
rionazv <70412011+rionazv@users.noreply.github.com>
2026-09-16 15:40:20 -0500

    Added a 2nd dropdown to accommodate new data types.

--- Unit-1-Final-Project/src/components/nav-components/NavDropdown.jsx
+++ Unit-1-Final-Project/src/components/nav-components/NavDropdown.jsx
@@ -13,8 +13,18 @@ export default function NavDropdown({setGalleryState}) {
 
         // CREATE THE DROPDOWN - FOR NOW, SELECTIONS ARE HARDCODED
         <div id="nav-dropdown">
+
+            <select name="image-category" id="image-category" className="dropdown-filter" onChange={handleChange}>
+
+                <option value="All">All Categories</option>
+                <option value="Wallpapers">Wallpapers</option>
+                <option value="CGs">CGs</option>
+                <option value="Stickers">Stickers</option>
+                <option value="No Results Test">No Results Test</option>
+
+            </select>
             
-            <select name="sticker-set" id="sticker-set" onChange={handleChange}>
+            <select name="image-set" id="image-set" className="dropdown-filter" onChange={handleChange}>
 
                 <option value="All">All Sticker Sets</option>
                 <option value="From Finality, the Origin">From Finality, the Origin</option>
