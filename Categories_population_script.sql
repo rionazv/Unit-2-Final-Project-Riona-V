b9df8c8a673eda041262d86fda437f2931e46cb1
Riona V. <70412011+rionazv@users.noreply.github.com>
2026-09-20 02:15:54 -0500

    Add files via upload
    Added a file that helps populate a database that is usually populated locally.

---
+++ Categories_population_script.sql
@@ -0,0 +1,4 @@
+INSERT INTO image_categories VALUES ("Wallpapers");
+INSERT INTO image_categories VALUES ("CGs");
+INSERT INTO image_categories VALUES ("Stickers");
+SELECT * FROM `unit-2-final-project`.image_categories;
