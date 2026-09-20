a333b8c8112b5f9e6e862cf746895ab57f494470
Riona V. <70412011+rionazv@users.noreply.github.com>
2026-09-20 13:53:54 -0500

    Updated DB population script

--- Categories_population_script.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO image_categories VALUES ("Wallpapers");
-INSERT INTO image_categories VALUES ("CGs");
-INSERT INTO image_categories VALUES ("Stickers");
-SELECT * FROM `unit-2-final-project`.image_categories;
---
+++ database_population_script.sql
@@ -0,0 +1,8 @@
+INSERT INTO image_categories VALUES ("Wallpapers");
+INSERT INTO image_categories VALUES ("CGs");
+INSERT INTO image_categories VALUES ("Stickers");
+INSERT INTO image_sets VALUES (1, "Test Set 1", 3);
+INSERT INTO images VALUES (1, "Fu Hua", "https://i.ibb.co/bM2p6PCm/hua5.webp", 1);
+SELECT * FROM `unit-2-final-project`.image_categories;
+SELECT * FROM `unit-2-final-project`.image_sets;
+SELECT * FROM `unit-2-final-project`.images;
